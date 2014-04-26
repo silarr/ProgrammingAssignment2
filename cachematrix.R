6a77fabc806f0db62aa2b1b7ebd6cb7e8d941ba7
silarr <silar.profile@gmail.com>
2014-04-26 14:57:28 -0700

    Added functionality to functions in the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## File contains two functions: 
+## 1) makeCacheMatrix - special type of "matrix" which contains a cached 
+## version of inversed matrix in it (if may be set to it manually or by 
+## second function "cacheSolve")
+## 2) cacheSolve - invert special type of "matrix" which is created by
+## makeCacheMatrix function and updates inner state of makeCacheMatrix matrix
+## by setting the value to it (makes a cache)
 
-## Write a short comment describing this function
-
+## create a special "matrix" which contains matrix value in it
+## and also cached version of inversed matrix if it is not null
 makeCacheMatrix <- function(x = matrix()) {
-test
+    s <- NULL
+    set <- function(y) {
+        x <<- y
+        s <<- NULL
+    }
+    get <- function() x
+    setsolution <- function(solution) s <<- solution
+    getsolution <- function() s
+    list(set = set, get = get,
+         setsolution = setsolution,
+         getsolution = getsolution)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+## also caches it inside x "matrix"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolution()
+    if(!is.null(s)) {
+        message("getting cached data")
+        return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolution(s)
+    s
 }
